511397340c8faccaf31c079174310748005acd34
M.RAY <a.mray47@yahoo.fr>
2021-06-24 16:47:09 +0800

    ready to commit

---
+++ config.py
@@ -0,0 +1,5 @@
+class Config():
+    FPS = 9
+    MENU_FPS = 60
+    WINDOW_WIDTH = 640
+    WINDOW_HEIGHT = 640
---
+++ main.py
@@ -0,0 +1,6 @@
+import pygame, sys
+
+pygame.init()
+
+DISPLAYSURF = pygame.set_model((800, 600))
+BASICFONT = pygame.font.Font('frees')
